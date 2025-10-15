af35be618f8b4e411bd352b8198c800ba95063c4
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-15 16:53:47 +0530

    Init

---
+++ main.lua
@@ -0,0 +1,22 @@
+local SceneryInit = require("src.libs.scenery")
+
+lg = love.graphics
+lk = love.keyboard
+la = love.audio
+
+
+local baseWidth = 1280
+local baseHeight = 720
+wW = love.graphics.getWidth()
+wH = love.graphics.getHeight()
+
+scale = math.max(wW / baseWidth, wH / baseHeight)
+
+local scenery = SceneryInit(
+    { path = "src.loader", key = "loader" },
+    { path = "src.open", key = "open" },
+    { path = "src.new", key = "new" }
+
+)
+
+scenery:hook(love)
