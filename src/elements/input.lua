3058d6958991034213a153d1e8a03dac4376dae5
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-21 12:59:06 +0530

    Created initi and textinput for input

--- src/elements/input.lua
+++ src/elements/input.lua
@@ -2,6 +2,28 @@ local input = {}
 
 input.__index = input
 
-function input:new(x, y, width, height)
+function input:new(x, y, width, padding, font, border)
     
+    self.x = x
+    self.y = y
+    self.width = x + width+ (padding * 2)
+    self.height = y + font:getHeight() + (padding * 2)
+    self.limit = width
+    self.font = font
+    self.border = border
+    self.text = {}
+    self.cursor = 1
+    
+
 end
+
+function input:textInput(t)
+    
+    table.insert(self.text, self.cursor, t) 
+    self.cursor = self.cursor + 1
+
+end
+
+
+
+return input
