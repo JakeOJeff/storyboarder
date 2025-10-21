42b6499c01b77d5bcfe5cd4f0aa31acc6dda5a32
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-21 13:10:00 +0530

    Fixed the input handler

--- src/elements/input.lua
+++ src/elements/input.lua
@@ -1,37 +1,32 @@
 local input = {}
-
 input.__index = input
 
 function input:new(x, y, width, padding, font, border)
-    
-    self.x = x
-    self.y = y
-    self.width = x + width+ (padding * 2)
-    self.height = y + font:getHeight() + (padding * 2)
-    self.limit = width
-    self.font = font
-    self.border = border
-    self.text = {}
-    self.cursor = 1
-    
-    return self
-
+    local obj = setmetatable({}, self)
+    obj.x = x
+    obj.y = y
+    obj.width = width + (padding * 2)
+    obj.height = font:getHeight() + (padding * 2)
+    obj.limit = width
+    obj.font = font
+    obj.border = border or 2
+    obj.padding = padding or 4
+    obj.text = {}
+    obj.cursor = 1
+    return obj
 end
 
 function input:textInput(t)
-    print(t)
-    table.insert(self.text, self.cursor, t) 
+    table.insert(self.text, self.cursor, t)
     self.cursor = self.cursor + 1
-
 end
 
 function input:draw()
-    love.graphics.setColor(1,1,1)
+    love.graphics.setColor(1, 1, 1)
     love.graphics.rectangle("fill", self.x, self.y, self.width, self.height, self.border)
-    love.graphics.setColor(0,0,0)
-    love.graphics.print(table.concat(self.text), self.x, self.y)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.setFont(self.font)
+    love.graphics.print(table.concat(self.text), self.x + self.padding, self.y + self.padding)
 end
-
-
 
 return input
--- src/loader.lua
+++ src/loader.lua
@@ -1,20 +1,18 @@
 local loader = {}
 local input = require("src.elements.input")
 
+local fileInput -- declare here
 
 function loader:load()
     fileInput = input:new(20, 20, 200, 5, love.graphics.getFont(), 3)
 end
 
-function loader:textInput(t)
+function loader:textinput(t)
     fileInput:textInput(t)
 end
 
 function loader:draw()
     fileInput:draw()
 end
-
-
-
 
 return loader
