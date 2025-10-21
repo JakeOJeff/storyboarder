cec432d11cb8fe7acdb58337b6a9907a2271e367
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-21 13:01:25 +0530

    Loading input from element lib

--- src/loader.lua
+++ src/loader.lua
@@ -1,6 +1,14 @@
 local loader = {}
+local input = require("elements.input")
 
 
+function loader:load()
+    fileInput = input:new(20, 20, 200, 5, love.graphics.getFont(), 3)
+end
+
+function loader:textInput(t)
+    
+end
 
 function loader:draw()
     
